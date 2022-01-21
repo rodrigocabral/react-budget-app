66ca63c4bb77b6c36aab9a6464f1cf707f012611
Rodrigo Cabral <rbatista.ti@gmail.com>
2022-01-21 15:43:20 -0300

    Use state to get values from budget form

--- src/components/AddBudgetModal.js
+++ src/components/AddBudgetModal.js
@@ -1,17 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import { useBudgets } from '../contexts/BudgetsContext';
 
 export default function AddBudgetModal({ show, handleClose }) {
-    const nameRef = useRef();
-    const maxRef = useRef();
+
+    const [frmBudgetState, setFrmBudgetState] = useState({
+        name: '',
+        max: '',
+    });
+
+    function handleChange({ target: { name, value }}) {
+        setFrmBudgetState(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+
     const { addBudget } = useBudgets();
 
     function handleSubmit(e) {
         e.preventDefault();
         addBudget({
-            name: nameRef.current.value,
-            max: parseFloat(maxRef.current.value),
+            name: frmBudgetState.name,
+            max: parseFloat(frmBudgetState.max),
         });
         handleClose();
     }
@@ -25,11 +36,11 @@ export default function AddBudgetModal({ show, handleClose }) {
                 <Modal.Body>
                     <Form.Group className='mb-3' controlId='name'>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control ref={nameRef} type='text' required />
+                        <Form.Control name='name' onChange={handleChange} type='text' required />
                     </Form.Group>
                     <Form.Group className='mb-3' controlId='max'>
                         <Form.Label>Maximum Spending</Form.Label>
-                        <Form.Control ref={maxRef} type='number' required min={0} step={0.01} />
+                        <Form.Control name='max' onChange={handleChange} type='number' required min={0} step={0.01} />
                     </Form.Group>
                     <div className='d-flex justify-content-end'>
                         <Button variant='primary' type='submit'>Add</Button>
