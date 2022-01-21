b68ec6ec43676ae15b4c06c7a898c9ed0c3f3ad6
Rodrigo Cabral <rbatista.ti@gmail.com>
2022-01-21 15:55:47 -0300

    Add button to show or hidde a budget modal and show a budget list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import { useState } from "react";
 import { Button, Container, Stack } from "react-bootstrap";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
+import { useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets, getAmountBudget } = useBudgets();
+
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <div style={{
-        display:'grid', 
-        gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-        gap: '1rem',
-        alignItems: 'flex-start',
-        }}>
-          <BudgetCard name='Enterteiment' amount={750} max={1000}></BudgetCard>
-        </div>
-    </Container>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div style={{
+          display:'grid', 
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+          }}>
+            {budgets.map(budget => {
+              const amount = getAmountBudget(budget.id);
+              return <BudgetCard key={budget.id} budgetId={budget.id} name={budget.name} amount={amount} max={budget.max}></BudgetCard>
+            })}
+          </div>
+      </Container>
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+    </>
   );
 }
 
