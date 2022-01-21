f453ae3d76583682bc9ba4937874b697f217d722
Rodrigo Cabral <rbatista.ti@gmail.com>
2022-01-21 15:54:18 -0300

    Add button to show or hidde a add expense modal

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,7 +1,10 @@
 import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
+import AddExpenseModal from "./AddExpenseModal";
+import { useState } from "react";
 
-export default function BudgetCard({name, amount, max, gray}) {
+export default function BudgetCard({name, amount, max, gray, budgetId}) {
+    const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
     const amountFormatted = currencyFormatter.format(amount);
     const maxFromatted = currencyFormatter.format(max);
     const bgClassNames = [];
@@ -13,27 +16,30 @@ export default function BudgetCard({name, amount, max, gray}) {
     }
 
     return (
-        <Card className={ bgClassNames.join(' ') }>
-            <Card.Body>
-                <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
-                    <div className="me-2">{name}</div>
-                    <div className="d-flex align-items-baseline">{amountFormatted} 
-                    <span className="text-muted fs-6 ms-1">/ {maxFromatted}</span></div>
-                </Card.Title>
-                <ProgressBar 
-                className="rounded-pill" variant={getProgressBarVariant(amount, max)}
-                min={0}
-                max={max}
-                now={amount}
-                />
-                <Stack direction="horizontal" gap={2} className="mt-4">
-                    <Button variant="outline-primary" className="ms-auto">
-                        Add Expense
-                    </Button>
-                    <Button variant="outline-secondary">View Expenses</Button>
-                </Stack>
-            </Card.Body>
-        </Card>
+        <>
+            <Card className={ bgClassNames.join(' ') }>
+                <Card.Body>
+                    <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+                        <div className="me-2">{name}</div>
+                        <div className="d-flex align-items-baseline">{amountFormatted} 
+                        <span className="text-muted fs-6 ms-1">/ {maxFromatted}</span></div>
+                    </Card.Title>
+                    <ProgressBar 
+                    className="rounded-pill" variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount}
+                    />
+                    <Stack direction="horizontal" gap={2} className="mt-4">
+                        <Button onClick={() => setShowAddExpenseModal(true)} variant="outline-primary" className="ms-auto">
+                            Add Expense
+                        </Button>
+                        <Button variant="outline-secondary">View Expenses</Button>
+                    </Stack>
+                </Card.Body>
+            </Card>
+            <AddExpenseModal budgetId={budgetId} show={showAddExpenseModal} handleClose={() => setShowAddExpenseModal(false)} />
+        </>
     )
 }
 
